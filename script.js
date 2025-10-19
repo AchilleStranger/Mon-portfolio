fbce84a4dc7fb0c300637b57605dd85117285b20
AchilleStranger <bossouachille99@gmail.com>
2025-10-19 19:10:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const themeBtn=document.getElementById('thème-btn');
+themeBtn.addEventListener('click',()=>{document.body.classList.toggle
+    ('dark-mode');
+});
+const navLinks=document.querySelectorAll('nav a');
+navLinks.forEach(link=>{
+    link.addEventListener('click', function(event){
+        event.preventDefault();
+        const targetId=this.getAttribute('href');
+        const targetSection=document.querySelector(targetId);
+        if(targetSection){
+            targetSection.scrollIntoView({
+                behavior:'smooth'
+            });
+        }
+    });
+});
